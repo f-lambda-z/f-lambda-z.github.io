de4b0b746a7ca9e9dade8e39c91e41a2748ce065
FΛZ <146303897+f-lambda-z@users.noreply.github.com>
2024-10-27 04:19:26 +0700

    chore: update file

--- change-head.sh
+++ change-head.sh
@@ -4,9 +4,16 @@ titleSite="Blog Pendidikan FΛZ"
 ################
 # Change title #
 ################
+# Categories homepage
 sed -i "s@<title>Categories</title>@<title>Kategori · ${titleSite}</title>@g" ./public/categories/index.html
+
+# Tags homepage
 sed -i "s@<title>Tags</title>@<title>Tag · ${titleSite}</title>@g" ./public/tags/index.html
+
+# Archives homepage
 sed -i "s@<title>Archives</title>@<title>Arsip · ${titleSite}</title>@g" ./public/archives/index.html
+
+# Posts homepage
 sed -i "s@<title>Posts</title>@<title>Postingan · ${titleSite}</title>@g" ./public/posts/index.html
 
 # Categories
@@ -35,3 +42,27 @@ sed -i 's@"description":"Blog Pendidikan"@"description":"Blog khusus pendidikan.
 sed -i 's@<meta property="og:description" content="Blog Pendidikan">@<meta property="og:description" content="Blog khusus pendidikan. Ayo, belajar dari blog Kami!!">@g' ./public/index.html
 sed -i 's@<meta name=description content="Blog Pendidikan">@<meta name="description" content="Blog khusus pendidikan. Ayo, belajar dari blog Kami!!">@g' ./public/index.html
 sed -i 's@<meta name=twitter:description content="Blog Pendidikan">@<meta name="twitter:description" content="Blog khusus pendidikan. Ayo, belajar dari blog Kami!!">@g' ./public/index.html
+
+# Tags homepage
+sed -i 's@"description":"Blog Pendidikan"@"description":"Blog khusus pendidikan. Ayo, belajar dari blog Kami!!"@g' ./public/tags/index.html
+sed -i 's@<meta property="og:description" content="Blog Pendidikan">@<meta property="og:description" content="Blog khusus pendidikan. Ayo, belajar dari blog Kami!!">@g' ./public/tags/index.html
+sed -i 's@<meta name=description content="Blog Pendidikan">@<meta name="description" content="Blog khusus pendidikan. Ayo, belajar dari blog Kami!!">@g' ./public/tags/index.html
+sed -i 's@<meta name=twitter:description content="Blog Pendidikan">@<meta name="twitter:description" content="Blog khusus pendidikan. Ayo, belajar dari blog Kami!!">@g' ./public/tags/index.html
+
+# Categories homepage
+sed -i 's@"description":"Blog Pendidikan"@"description":"Blog khusus pendidikan. Ayo, belajar dari blog Kami!!"@g' ./public/categories/index.html
+sed -i 's@<meta property="og:description" content="Blog Pendidikan">@<meta property="og:description" content="Blog khusus pendidikan. Ayo, belajar dari blog Kami!!">@g' ./public/categories/index.html
+sed -i 's@<meta name=description content="Blog Pendidikan">@<meta name="description" content="Blog khusus pendidikan. Ayo, belajar dari blog Kami!!">@g' ./public/categories/index.html
+sed -i 's@<meta name=twitter:description content="Blog Pendidikan">@<meta name="twitter:description" content="Blog khusus pendidikan. Ayo, belajar dari blog Kami!!">@g' ./public/categories/index.html
+
+# Archives homepage
+sed -i 's@"description":"Blog Pendidikan"@"description":"Kumpulan arsip dari konten-konten yang Kami posting."@g' ./public/archives/index.html
+sed -i 's@<meta name=description content="Blog Pendidikan">@<meta name="description" content="Kumpulan arsip dari konten-konten yang Kami posting.">@g' ./public/archives/index.html
+sed -i 's@<meta property="og:description" content="Blog Pendidikan">@<meta property="og:description" content="Kumpulan arsip dari konten-konten yang Kami posting.">@g' ./public/archives/index.html
+sed -i 's@<meta name=twitter:description content="Blog Pendidikan">@<meta name="twitter:description" content="Kumpulan arsip dari konten-konten yang kami posting.">@g' ./public/archives/index.html
+
+# Posts homepage
+sed -i 's@"description":"Blog Pendidikan"@"description":"Blog khusus pendidikan. Ayo, belajar dari blog Kami!!"@g' ./public/posts/index.html
+sed -i 's@<meta property="og:description" content="Blog Pendidikan">@<meta property="og:description" content="Blog khusus pendidikan. Ayo, belajar dari blog Kami!!">@g' ./public/posts/index.html
+sed -i 's@<meta name=description content="Blog Pendidikan">@<meta name="description" content="Blog khusus pendidikan. Ayo, belajar dari blog Kami!!">@g' ./public/posts/index.html
+sed -i 's@<meta name=twitter:description content="Blog Pendidikan">@<meta name="twitter:description" content="Blog khusus pendidikan. Ayo, belajar dari blog Kami!!">@g' ./public/posts/index.html
